60eb47ac5fa4a2cfd15959fc02e7284084f8e3b7
Daniel Nunes <danielnunes17@gmail.com>
2022-02-28 15:41:19 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+var sb = new StringBuilder();
+sb.AppendLine("O rato roeu a roupa do Rei de Roma");
+sb.AppendLine("No ninho de mafagafa tinham 3 mafagafinhas");
+sb.AppendLine("Quem tem medo do lobo mau, lobo mau, lobo mau?");
+
+using var sr = new StringReader(sb.ToString());
+
+do
+{
+      Console.WriteLine(sr.ReadLine());
+
+} while (sr.Peek() >= 0);
+
+Console.WriteLine("Aperte [ENTER] para finalizar");
+Console.ReadKey();
+{
+
+
+}
